c0c8225059f87e1b0d15c6a719cc0952ac57b257
paru <paru@sqnya.se>
2015-09-13 21:08:41 +0200

    Sped up flood fill

--- src/core/parupaintFrame.cpp
+++ src/core/parupaintFrame.cpp
@@ -66,9 +66,11 @@ void ParupaintFrame::DrawStep(float x, float y, float x2, float y2, float width,
 	pen.setCapStyle(Qt::RoundCap);
 	this->DrawStep(x, y, x2, y2, pen);
 }
+#include <QRgb>
 
 void p_fill(QImage & img, int x, int y, QRgb orig, QRgb to)
 {
+	if(img.format() != QImage::Format_ARGB32) return;
 	if(orig == to) return;
 
 	const QRect r = img.rect();
@@ -77,37 +79,46 @@ void p_fill(QImage & img, int x, int y, QRgb orig, QRgb to)
 	QRgb pp = img.pixel(x, y);
 	if(pp != orig) return;
 
+	const int ww = r.size().width();
+	const int hh = r.size().height();
 	QList<QPoint> plist = {QPoint(x, y)};
-	const auto ww = r.width();
+	plist.reserve(ww*hh);
 
+	uchar * img_bits = img.bits();
+	uchar * img_data = new uchar[img.byteCount()];
+	memcpy(img_data, img_bits, img.byteCount());
+
+	// A B G R
 	while(!plist.isEmpty()){
-		auto w = plist.front(), e = w;
 
-		while(w.x() > 0){
-			w.setX(w.x()-1);
-			if(img.pixel(w) != orig){
-				w.setX(w.x() + 1);
+		const QPoint p = plist.front();
+		const int ty = p.y();
+		int tx = p.x(), tx2 = tx;
+
+		while(tx > 0){
+			tx--;
+			if(*(QRgb*)(img_data + (4 * (tx + (ty * ww)))) != orig){
+				tx++;
 				break;
 			}
 		}
-		while(e.x() < ww){
-			e.setX(e.x()+1);
-			if(e.x() >= ww) break; // .pixel(.width) doesn't work for some reason
-			if(img.pixel(e) != orig){
+		while(tx2 < ww){
+			tx2++;
+			if(tx2 >= ww) break; // .pixel(.width) doesn't work for some reason
+			if(*(QRgb*)(img_data + (4 * (tx2 + (ty * ww)))) != orig){
 				break;
 			}
 		}
-		for(int x = w.x(); x < e.x(); x++){
-			int px = x;
-			int py = plist.front().y();
 
-			img.setPixel(px, py, to);
-			if(py > 0 && img.pixel(px, py - 1) == orig) plist.append(QPoint(px, py-1));
-			if(py < r.height()-1 && img.pixel(px, py + 1) == orig) plist.append(QPoint(px, py+1));
+		for(int x = tx; x < tx2; x++){
+			*(QRgb*)(img_data + (4 * (x + (ty * ww)))) = to;
+			if(ty > 0 && *(QRgb*)(img_data + (4 * (x + ((ty - 1) * ww)))) == orig) { plist.append(QPoint(x, ty-1)); }
+			if(ty < hh-1 && *(QRgb*)(img_data + (4 * (x + ((ty + 1) * ww)))) == orig) { plist.append(QPoint(x, ty+1)); }
 		}
 		plist.pop_front();
 
 	}
+	img = QImage(img_data, ww, hh, img.format());
 }
 
 void ParupaintFrame::Fill(int x, int y, QColor color)
