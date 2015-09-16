b3c1666aa18ae0d61f9a6f4010a0c7fa5c6ae2cf
paru <paru@sqnya.se>
2015-09-16 20:36:45 +0200

    U+200B padding

--- src/overlay/parupaintChatContent.cpp
+++ src/overlay/parupaintChatContent.cpp
@@ -19,8 +19,9 @@ ParupaintChatContent::ParupaintChatContent(QWidget * parent) : QTextBrowser(pare
 {
 	this->setObjectName("ChatContent");
 	this->setFocusPolicy(Qt::ClickFocus);
-	this->setStyleSheet("margin:0; padding:0; border:none; background-color:transparent;");
+	this->setStyleSheet("margin:0; padding:0; border:none; background:transparent;");
 	this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	this->document()->setDocumentMargin(0);
 
 	QFile file(":resources/chat.css");
 	file.open(QFile::ReadOnly);
@@ -38,17 +39,26 @@ void ParupaintChatContent::AddMessage(QString msg, QString who)
 {
 	// if it's a system message, don't escape
 	// TODO make sure you can't join without a valid name
-	QString str = "<span class=\"message\">" + (who.isEmpty() ? msg : msg.toHtmlEscaped()) + "</span>";
+	QString str = "<span class=\"chat-message\">" + (who.isEmpty() ? msg : " " + msg.toHtmlEscaped()) + "</span>";
 	if(!who.isEmpty()){
-		str.prepend("<span class=\"user\"> " + who.toHtmlEscaped() + " </span>: ");
+		str.prepend("<span class=\"chat-user\">" + who.toHtmlEscaped() + ":</span>");
 	}
 	this->AddChatMessage(str);
 }
 
 void ParupaintChatContent::AddChatMessage(QString str)
 {
-	QString html = QString("<div class=\"chatmessage\">%1</div>\n\r").arg(str);
-	this->append(html);
+	// i'm proud of myself for this one!
+	// ok, story time: i needed to get some padding in to .chat-entry.
+	// tried settings padding, didn't work. hmm... seems like qt only
+	// supports padding on table cells. well no problem, let's use cells!
+	// ... but the cells inserts newlines however it wants! everywhere!
+	// not good, hmm, what else can i do... QTextOption?... looking
+	// through the qt sources... nope nothing...
+	// IDEA! i could just insert an invisible character at the beginning
+	// and end, adjust their size and give at least some vertical padding.
+	// perfect!!!
+	this->append(str.prepend("<div class=\"chat-entry\">\u200b").append("\u200b</div>"));
 }
 void ParupaintChatContent::Scroll(int x, int y)
 {
@@ -71,13 +81,15 @@ void ParupaintChatContent::contextMenuEvent(QContextMenuEvent* e)
 	auto * menu = this->createStandardContextMenu();
 	QAction * action = new QAction("&Save to log file...", this);
 	connect(action, &QAction::triggered, [&menu, this](bool){
-		QDateTime time = QDateTime::currentDateTime();
-		QString filename = "chatlog_at_" + time.toString("yyyy-MM-dd_HH.mm.ss") + ".html";
-		QString file = QFileDialog::getSaveFileName(this, "Save log file as...", "./" + filename, "HTML Files (*.html);; Text files (*.txt)");
-
+		QString name = "chatlog_at_" + QDateTime::currentDateTime().toString("yyyy-MM-dd_HH.mm.ss");
+		this->setDocumentTitle(name);
+		QString file = QFileDialog::getSaveFileName(this, "Save log file as...", "./" + this->documentTitle() + ".html", "HTML Files (*.html);; Text files (*.txt)");
 		QFile file_write(file);
 		if(file_write.open(QIODevice::WriteOnly)){
-			QString text = file.endsWith(".html", Qt::CaseInsensitive) ? this->toHtml() : this->toPlainText();
+			QString text = (file.endsWith(".html", Qt::CaseInsensitive) ?
+					this->toHtml() :
+					this->toPlainText()).replace("\u200b", "");
+			qDebug() << text;
 			file_write.write(text.toUtf8());
 			file_write.close();
 		}
