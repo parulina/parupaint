b89d13f6fedcce88567baca89660a0db2ae3f1a2
paru <paru@sqnya.se>
2016-02-21 14:34:28 +0100

    Beta version 0.925

--- src/parupaintVersion.h
+++ src/parupaintVersion.h
@@ -1,6 +1,6 @@
 #define PARUPAINT_MAJOR_VERSION 0
 #define PARUPAINT_MINOR_VERSION 9
-#define PARUPAINT_PATCH_VERSION 2
+#define PARUPAINT_PATCH_VERSION 25
 
 #define PARUPAINT_VERSION__(ma,mi,pa) #ma "." #mi #pa
 #define PARUPAINT_VERSION_(ma,mi,pa) PARUPAINT_VERSION__(ma, mi, pa)
